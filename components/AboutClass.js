73b3a91da69f9c5c287dc5b54303403869b71f97
“shiiijo” <shrinidhijoshi99@gmail.com>
2023-06-16 22:16:30 +0530

    how to create state variables in class based componenets

--- components/AboutClass.js
+++ components/AboutClass.js
@@ -14,11 +14,21 @@ import React from "react";
 //same thing is implemented using class based-component
 // Coomponent is the react class from which our is inheriting all the properties
 // we are using Coomponent class from react package like this React.Component
+
 class AboutClass extends React.Component {
   // to receive props from outside we need to add constructor for this class
-  // "this" keyword and "super(props)" are compulsary to use 
+  // "this" keyword and "super(props)" are compulsary to use
+
   constructor(props) {
     super(props);
+    // class based componenets is old way , when react built initially class based compon were in use
+    // that time hooks were not introduced , so before hooks this is how we used to create state variables
+    // mutiple state variables can be defined in this state object
+    // behind the scenes react stores state variables like this in case of hooks
+    this.state = {
+      count1: 0,
+      count2: 1,
+    };
   }
 
   render() {
