c37cb467cf3c78d1369342c05c71acb776c7f6be
“shiiijo” <shrinidhijoshi99@gmail.com>
2023-06-17 19:38:00 +0530

    added back functional componenet for about page

--- App.js
+++ App.js
@@ -4,10 +4,11 @@ import { Header } from "./components/Header";
 import { Body } from "./components/Body";
 import { Footer } from "./components/Footer";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-import  AboutClass  from "./components/AboutClass";
+// import AboutClass from "./components/AboutClass";
 import { Contact } from "./components/Contact";
 import { Error } from "./components/Error";
 import { RestaurantMenu } from "./components/RestaurantMenu";
+import { About } from "./components/About";
 
 const AppLayout = () => {
   return (
@@ -29,9 +30,10 @@ const appRouter = createBrowserRouter([
         element: <Body />,
       },
       {
-        // example of class based component 
+        // converted to function componnent - to study class based compo
+        // refer this componenet AboutClass.js
         path: "/about",
-        element: <AboutClass name={"Shrinidhi Joshi"} />,
+        element: <About />,
       },
       {
         path: "/contact",
---
+++ components/About.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+export const About = () => {
+  const [avatar, setAvatar] = useState("avatar");
+  const [name, setName] = useState("name");
+  const [bio, setBio] = useState("bio");
+  const [twitter, setTwitter] = useState("twitter handle");
+
+  useEffect(async function () {
+    let response = await fetch("https://api.github.com/users/shiiijo");
+    let json = await response.json();
+    console.log(json);
+    setAvatar(json.avatar_url);
+    setName(json.name);
+    setBio(json.bio);
+    setTwitter(json.twitter_username);
+
+    return () =>{
+        // do un-mount of the things if needed
+    }
+  }, []);
+
+  return (
+    <div className="about-card">
+      <img src={avatar}></img>
+      <h4>{name}</h4>
+      <h4>{bio}</h4>
+      <h4>Email : shrinidhijoshi99@gmail.com</h4>
+      <h4>Twitter : {twitter}</h4>
+    </div>
+  );
+};
