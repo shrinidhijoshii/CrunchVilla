bb6f14740cb0f43bf7c502266226ab7bf34acad8
“shiiijo” <shrinidhijoshi99@gmail.com>
2023-06-21 21:43:58 +0530

    trigger unmount cycle for event-listners : senior developer way

--- utils/hooks/useOnline.js
+++ utils/hooks/useOnline.js
@@ -6,13 +6,22 @@ const useOnline = () => {
   // we want to check online or offline only once page is rendered
   // so we are using useEffect with dependency array
   useEffect(() => {
-    window.addEventListener("online", () => {
+    const handleOnline = () => {
       setIsOnline(true);
-    });
+    };
 
-    window.addEventListener("offline", () => {
+    const handleOffline = () => {
       setIsOnline(false);
-    });
+    };
+
+    window.addEventListener("online", handleOnline);
+
+    window.addEventListener("offline", handleOffline);
+
+    return () => {
+      window.removeEventListener("online", handleOnline);
+      window.removeEventListener("offline", handleOffline);
+    };
   }, []);
 
   return isOnline;
