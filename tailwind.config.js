a5253a491f50117fe5923c32164fc2692c8f0262
“shiiijo” <shrinidhijoshi99@gmail.com>
2023-06-24 18:39:46 +0530

    tailwind one time configuration

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // It will take paths which will use tailwind css
+  content: ["./**/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
