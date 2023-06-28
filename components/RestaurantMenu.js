5719f6cf0b95555755ae23907be6f57175f1f373
“shiiijo” <shrinidhijoshi99@gmail.com>
2023-06-28 14:53:35 +0530

    impl menu details feature

--- components/RestaurantMenu.js
+++ components/RestaurantMenu.js
@@ -4,21 +4,25 @@ import { useParams } from "react-router-dom";
 import useRestaurant from "../utils/hooks/useRestaurant";
 
 export const RestaurantMenu = () => {
-
   // useParams is hook which gives parameters passed from the request url
-  // params variable holds object , object will have key:value and key will be the name given 
-  // in route 
+  // params variable holds object , object will have key:value and key will be the name given
+  // in route
   const params = useParams();
-  console.log(params.id)
+  console.log(params.id);
 
   const resInfo = useRestaurant(params.id);
-  console.log(resInfo)
+  console.log(resInfo);
 
   if (resInfo == null) {
     return <Shimmer />;
   }
-  const { name, cuisines, costForTwoMessage, avgRatingString } =
-    resInfo.cards[0].card.card.info;
+  const {
+    name,
+    cuisines,
+    costForTwoMessage,
+    avgRatingString,
+    cloudinaryImageId,
+  } = resInfo.cards[0].card.card.info;
 
   const { itemCards } =
     resInfo.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card;
@@ -26,17 +30,36 @@ export const RestaurantMenu = () => {
 
   return (
     <div className="menu">
-      <h1>{name}</h1>
-      <h3>{cuisines.join(" , ")}</h3>
-      <h3>{costForTwoMessage}</h3>
-      <h3>{avgRatingString}</h3>
+      <div className="text-center text-xl font-semibold">
+        <img
+          src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
+          className="h-80 ml-auto mr-auto"
+        ></img>
+        <h1>{name}</h1>
+        <h3>{cuisines.join(" , ")}</h3>
+        <h3>{costForTwoMessage}</h3>
+        <h3>{avgRatingString}</h3>
+        <hr></hr>
+      </div>
       <div className="menu-items">
-        <ul>
+        <h1 className="text-center text-lg">Menu Items</h1>
+        <ul className="flex flex-wrap">
           {itemCards.map((item) => (
-            <li key={item.card.info.id}>
+            <li
+              key={item.card.info.id}
+              className="m-10 text-center shadow-md w-80 ml-auto mr-auto"
+            >
               {" "}
-              {item.card.info.name} - Rs{" "}
-              {item.card.info.price / 100 || item.card.info.defaultPrice / 100}{" "}
+              <img
+                src={`https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${item.card.info.imageId}`}
+                className="h-40 ml-auto mr-auto"
+              ></img>
+              <p className="font-semibold">
+                {item.card.info.name} - Rs{" "}
+                {item.card.info.price / 100 ||
+                  item.card.info.defaultPrice / 100}{" "}
+              </p>
+              <p>{item.card.info.description}</p>
             </li>
           ))}
         </ul>
