f4b9b5a1e0ea4e489cb90926d4ae2c47a07a14d6
“shiiijo” <shrinidhijoshi99@gmail.com>
2023-07-01 17:51:43 +0530

    how to add slice in the redux store

--- utils/store.js
+++ utils/store.js
@@ -1,5 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./cartSlice";
 
-const store = configureStore({});
+const store = configureStore({
+    // put slice into the store
+    reducer:{
+        cart : cartSlice,
+    }
+});
+
+
+/*
+- reducer will take name of slice you defined in create slice as a key and
+value will be created slice name eg , cart : cartSlice
+- name refers to the name defined in createSlice api like this  "name: "cart"
+- cartSlice is exported by default so it will contain all the reducers
+*/
 
 export default store;
