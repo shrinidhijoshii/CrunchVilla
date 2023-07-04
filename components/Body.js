05b671204a213540f240024735476f62c733328f
“shiiijo” <shrinidhijoshi99@gmail.com>
2023-07-04 23:26:10 +0530

    pathch - ignore fixed typo

--- components/Body.js
+++ components/Body.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
 import ReactDOM from "react-dom/client";
-import { useEffect, useState } from "react";
 import { Card } from "./Card";
 import { Shimmer } from "./Shimmer";
 import { Link } from "react-router-dom";
