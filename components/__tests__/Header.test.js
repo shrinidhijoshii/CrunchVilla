4724587be4dd44ea9290a3a023ebd26c6b2bf220
“shiiijo” <shrinidhijoshi99@gmail.com>
2023-07-05 22:51:39 +0530

    discussion about things required to run test for a component

--- components/__tests__/Header.test.js
+++ components/__tests__/Header.test.js
@@ -1,6 +1,17 @@
-import { render } from "@testing-library/react"
-import { Header } from "../Header"
+import { render } from "@testing-library/react";
+import { Header } from "../Header";
+import { Provider } from "react-redux";
+import store from "../../utils/store";
+import { StaticRouter } from "react-router-dom/server";
 
-test("Check header logo on page render",() => {
-    const header = render(<Header/>);
-})
+test("Check header logo on page render", () => {
+  const header = render(
+    <StaticRouter>
+      <Provider store={store}>
+        <Header />
+      </Provider>
+    </StaticRouter>
+  );
+  // header holds the virtual dom (js object of the component)
+  console.log(header)
+});
